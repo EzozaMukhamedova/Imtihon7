810de8d889cbe715c958241b8e480ab447c8b95a
EzozaMukhamedova <ezoza.muxamedova@gmail.com>
2025-03-14 02:53:14 +0900

    dashboard kansei

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,29 +5,48 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-    <div className="bg-[#343a40] ">
-    <div className="bg-[#343a40] w-[1440px] m-auto flex items-center justify-between  h-[70px] text-white">
-        <div className="flex ">
-          <p className="flex items-center text-white">
-            <RiCodeSSlashLine  className="w-[25px] h-[25px] font-bold"/>
-            <span className="text-[25px] font-[700]"> DevConnector</span>
-          </p>
+      <div className="bg-[#343a40]  border-b-[1px] border-b-[#17A2B8]">
+        <div className="bg-[#343a40] w-[1440px] m-auto flex items-center justify-between  h-[61px] text-white">
+          <div className="flex hover:text-[#17a2b8] cursor-pointer">
+            <p className="flex items-center text-white hover:text-[#17a2b8]">
+              <RiCodeSSlashLine className="w-[25px] h-[25px] stroke-[1]" />
+
+              <span className="pl-[10px] text-[25px] font-[700] ">
+                {" "}
+                DevConnector
+              </span>
+            </p>
+          </div>
+          <ul className="flex">
+            <li>
+              <NavLink
+                to={"/"}
+                className="text-white p-[8px] text-[17px] hover:text-[#17a2b8]"
+              >
+                Developers
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/register"
+                className="text-white p-[8px] text-[17px] hover:text-[#17a2b8]"
+              >
+                Register
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                to="/login"
+                className="text-white pl-[8px] text-[17px] hover:text-[#17a2b8]"
+              >
+                Login
+              </NavLink>
+            </li>
+          </ul>
         </div>
-        <ul className="flex">
-          <li>
-            <NavLink to={"/"} className="text-white p-[8px] text-[18px] hover:text-[#17a2b8]">Developers</NavLink>
-          </li>
-          <li>
-            <NavLink to="/register"  className="text-white p-[8px]  text-[18px] hover:text-[#17a2b8]">Register</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login"  className="text-white p-[8px] text-[18px] hover:text-[#17a2b8]">Login</NavLink>
-          </li>
-        </ul>
       </div>
-    </div>
-      
-    </> 
+    </>
   );
 };
 
--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,25 +1,40 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../App.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className="relative w-full h-[729px]">
-        <Navbar />
-        <div className="dark-overlay "></div>
+      <div className="relative w-full h-screen overflow-hidden">
+        <div className="absolute top-0 left-0 z-10 w-full">
+          {" "}
+          <Navbar />
+        </div>
+
+        <div className="absolute top-0 left-0 w-full h-full dark-overlay"></div>
 
         <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full text-center text-white ">
-          <h1 className="text-[64px]">Developer Connector</h1>
+          <h1 className="text-[64px] font-bold mb-[10px]">
+            Developer Connector
+          </h1>
           <h2 className="text-[24px]">
             Create a developer profile/portfolio, share posts and get help from
             other developers
           </h2>
-          <div className="flex gap-4 mt-4">
-            <button className="px-[21px] py-[8px] bg-[#17A2B8] text-amber-50 rounded">
-              Sign in
+          <div className="flex gap-2 mt-4">
+            <button
+              onClick={() => navigate("/register")}
+              className="px-[21px] py-[8px] bg-[#17A2B8] text-amber-50  cursor-pointer transition-all duration-300 hover:bg-[#17a3b8bd]"
+            >
+              Sign Up
             </button>
-            <button className="px-[21px] py-[8px] bg-[#f4f4f4] rounded">
+            <button
+              onClick={() => navigate("/login")}
+              className="px-[21px] py-[8px] bg-[#f4f4f4] hover:bg-[#f4f4f4c1] transition-all duration-300 text-black cursor-pointer"
+            >
               Login
             </button>
           </div>
