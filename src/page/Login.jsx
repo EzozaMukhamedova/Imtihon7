00b7a37cd06a0cb7772dc247b1a82d81083a9414
EzozaMukhamedova <ezoza.muxamedova@gmail.com>
2025-04-06 13:32:22 +0900

    login register

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -50,19 +50,19 @@ const Login = () => {
 
   return (
     <>
-      <Navbar />
-      <div className="flex flex-col ">
-        <h2 className="text-[#4F4F4F] text-[36px] font-bold text-center">
+      {/* <Navbar /> */}
+      <div className="flex flex-col border bg-[#FCFAFA] w-[1440px] mx-auto pt-[98px] pb-[125px]">
+        <h2 className="text-[#4F4F4F] text-[36px] font-bold text-center ">
           Welcome, Log into you account
         </h2>
-        <div className="p-4 mx-auto bg-white rounded-lg">
+        <div className=" mx-auto bg-white border rounded-[5px] mt-[50px] px-[134px]">
           {error && <p className="text-center text-red-500">{error}</p>}
-          <div className="flex items-center py-2 ">
-            <h2 className="text-xl py-[16px] text-center text-[16px] mt-[125px] mb-[14px]">
+          <div className="text-center ">
+            <h2 className="text-[16px] text-gray-600 text-center mt-18 mb-3.5 leading-normal">
               It is our great pleasure to have <br /> you on board!{" "}
             </h2>
           </div>
-          <form onSubmit={handleSubmit} className="flex flex-col">
+          <form onSubmit={handleSubmit} className="flex flex-col ">
             <input
               type="email"
               name="email"
@@ -81,24 +81,24 @@ const Login = () => {
               className="w-[248px] h-[42px] rounded-[4px] p-1 mb-2 text-[14px] border border-gray-300 outline-none "
               required
             />
+
             <button
               type="submit"
-              className="w-[248px] h-[42px] rounded-[4px]  bg-[#2D88D4] text-white py-2 mt-[15px] hover:bg-[#138a9c] transition cursor-pointer"
+              className="w-[248px] h-[42px] rounded-[4px]  bg-[#2D88D4] text-white py-2 mt-4 cursor-pointer transition duration-300 ease-in-out transform hover:bg-[#1a6ca7] active:scale-95"
             >
               Login
             </button>
+            <div className="bg-white ml-[15px] mt-4 mb-[40px]">
+              <p>
+                <span
+                  className="text-[#2D88D4] font-bold cursor-pointer text-center flex justify-center transition duration-300 ease-in-out hover:text-[#1a6ca7] hover:underline"
+                  onClick={() => navigate("/register")}
+                >
+                  Sign Up
+                </span>
+              </p>
+            </div>
           </form>
-        </div>
-
-        <div className="bg-white ml-[15px]">
-          <p>
-            <span
-              className="text-[#2D88D4] font-bold cursor-pointer text-center flex justify-center"
-              onClick={() => navigate("/register")}
-            >
-              Sign Up
-            </span>
-          </p>
         </div>
       </div>
     </>
--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -3,7 +3,7 @@ import { BiShow, BiHide } from "react-icons/bi"; // 👁️
 import AuthContext from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { register } from "../service/authService";
-import { FaUser } from "react-icons/fa";
+
 import Navbar from "../components/Navbar";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -70,85 +70,100 @@ const Register = () => {
 
   return (
     <>
-      <Navbar />
-      <div className="flex flex-col ">
-
-        <div className="p-4 mx-auto bg-white ">
-          <h2 className="text-[#4F4F4F] text-center text-[36px]  font-bold stroke-[3]">
-            Welcome, Sign up
-          </h2>
-          <div className="flex items-center py-4 ml-9 mt-[125px] mb-[34px]">
-            <h2 className="text-[16px] text-[#667085] text-center">
-              It is our great pleasure to have <br /> you on board!{" "}
+      {/* <Navbar /> */}
+      <div className="flex flex-col bg-[#FCFAFA] border">
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="mx-auto border">
+            {/* mt-[98px] mb-[98px] */}
+            <h2 className="text-[#4F4F4F] text-center text-[36px]  font-bold stroke-[3] ">
+              Welcome, Sign up
             </h2>
+            <div className="px-[130px] py-[70px] border bg-white rounded-[4px] mt-[10px]">
+              <div className="flex items-center ml-9  mb-[34px] ">
+                <h2 className="text-[16px] text-[#667085] text-center">
+                  It is our great pleasure to have <br /> you on board!{" "}
+                </h2>
+              </div>
+
+              {error && <p className="ml-4 text-red-500">{error}</p>}
+
+              <form onSubmit={handleSubmit} className="flex flex-col ">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-[248px] h-[42px] rounded-[4px] pl-[6px] p-1 outline-none ml-4 text-[14px] border border-gray-300  mb-4 "
+                  required
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-[248px] h-[42px] rounded-[4px] pl-[6px] p-1 outline-none ml-4 text-[14px] border border-gray-300  mb-[14px] "
+                  required
+                />
+
+                <div className="relative">
+                  {/* <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  placeholder="Password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="w-[248px] h-[42px] rounded-[4px] pl-[6px] p-1 ml-4 outline-none text-[14px] border border-gray-300  mb-4 pr-10"
+                  required
+                />
+                <button
+                  type="button"
+                  className="absolute right-[30px] top-[13px] cursor-pointer"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <BiHide /> : <BiShow />}{" "}
+                </button> */}
+                </div>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    placeholder="Create your Password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    className="w-[248px] h-[42px] rounded-[4px] pl-[6px] p-1 ml-4 outline-none text-[14px] border border-gray-300  mb-2 pr-10"
+                    required
+                  />
+                  <button
+                    type="button"
+                    className="absolute right-[30px] top-[13px] cursor-pointer"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <BiHide /> : <BiShow />}{" "}
+                  </button>
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-[248px] h-[42px] rounded-[4px] ml-4 bg-[#2D88D4] text-white py-2 mt-4 cursor-pointer transition duration-300 ease-in-out transform hover:bg-[#1a6ca7] active:scale-95"
+                >
+                  Sign Up
+                </button>
+              </form>
+
+              <div className="bg-white ml-[15px] mt-[20px]">
+                <p>
+                  <span
+                    className="text-[#2D88D4] font-bold cursor-pointer text-center flex justify-center transition duration-300 ease-in-out hover:text-[#1a6ca7] hover:underline"
+                    onClick={() => navigate("/login")}
+                  >
+                    Login
+                  </span>
+                </p>
+              </div>
+            </div>
           </div>
-
-          {error && <p className="ml-4 text-red-500">{error}</p>}
-
-          <form onSubmit={handleSubmit} className="flex flex-col ">
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={formData.name}
-              onChange={handleChange}
-              className="w-[248px] h-[42px] rounded-[4px] pl-[6px] p-1 outline-none ml-4 text-[19px] border border-gray-300  mb-4 "
-              required
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Enter your Email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-[248px] h-[42px] rounded-[4px] pl-[6px] p-1 outline-none ml-4 text-[14px] border border-gray-300  mb-[14px] "
-              required
-            />
-
-            <div className="relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                name="password"
-                placeholder="Password"
-                value={formData.password}
-                onChange={handleChange}
-                className="w-[248px] h-[42px] rounded-[4px] pl-[6px] p-1 ml-4 outline-none text-[14px] border border-gray-300  mb-4 pr-10"
-                required
-              />
-              <button
-                type="button"
-                className="absolute right-[40px] top-[13px] cursor-pointer"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <BiHide /> : <BiShow />}{" "}
-              </button>
-            </div>
-            <div className="relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                name="password"
-                placeholder="Create your Password"
-                value={formData.password}
-                onChange={handleChange}
-                className="w-[248px] h-[42px] rounded-[4px] pl-[6px] p-1 ml-4 outline-none text-[14px] border border-gray-300  mb-2 pr-10"
-                required
-              />
-              <button
-                type="button"
-                className="absolute right-[40px] top-[13px] cursor-pointer"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <BiHide /> : <BiShow />}{" "}
-              </button>
-            </div>
-
-            <button
-              type="submit"
-              className="w-[248px] h-[42px] rounded-[4px] ml-4 bg-[#2D88D4] text-white py-2 mt-4 cursor-pointer"
-            >
-              Sign Up
-            </button>
-          </form>
         </div>
       </div>
     </>
